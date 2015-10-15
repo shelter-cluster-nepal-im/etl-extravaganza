599141b81a28f343d0b794d1afa6d906fdb3b182
eoglethorpe <ewanogle@gmail.com>
2015-10-15 13:37:30 +0545

    logs now in csv format, exql restruct

--- etl/clean.py
+++ etl/clean.py
@@ -12,13 +12,17 @@ from dateutil.parser import *
 from openpyxl.cell import column_index_from_string
 from datetime import datetime
 
+def return_message(message, list):
+    """return message and list"""
+    return [message, list]
+
 def algo1(db, ref):
     #***Column A must be in Reference>ImplementingAgency if not: make a note
     db_col_loc = etl.find_in_header(db, 'Implementing agency')
     ref_col_loc = etl.find_in_header(ref,'Implementing_Agency_Name')
     missing_names = etl.colvals_notincol(db, db_col_loc, ref, ref_col_loc)
 
-    return db, ref, 'Agencies not in the reference:\n' + ','.join(missing_names)
+    return db, ref, return_message('Agencies not in the reference:' ,missing_names)
 
 def algo2(db,ref):
     #Column B: If == Implementing Agency set Column B=Internal
@@ -27,12 +31,13 @@ def algo2(db,ref):
     db_col_loc_source = etl.find_in_header(db, 'Sourcing Agency')
     vals_changed = []
 
-    for row in db.iter_rows(db_col_loc_impl + "2:" + 
+    for row in db.iter_rows(db_col_loc_impl + "2:" +
         etl.find_last_value(db, db_col_loc_source, 'c')):
             if row[0].value == row[1].value:
                 vals_changed.append(etl.xstr(row[1].value))
                 row[1].value = 'INTERNAL'
-    return db, ref, 'Sourcing Agencies set as INTERNAL:\n' + ','.join(vals_changed)
+
+    return db, ref, return_message('Sourcing Agencies set as INTERNAL:' ,vals_changed)
 
 
 def algo3(db,ref):
@@ -44,7 +49,7 @@ def algo3(db,ref):
     ref_col_loc = etl.find_in_header(ref,'Local_Partner_Agency')
     missing_names = etl.colvals_notincol(db, db_col_loc, ref, ref_col_loc)
 
-#    return db, ref, '\n' + ','.join()
+#    return db, ref, return_message('' ,)
 
 def algo4(db,ref):
     #if Column C == Column A
@@ -54,13 +59,13 @@ def algo4(db,ref):
     db_col_loc_c = etl.find_in_header(db, 'Local partner agency')
     vals_changed = []
 
-    for row in db.iter_rows(db_col_loc_a + "2:" + 
+    for row in db.iter_rows(db_col_loc_a + "2:" +
         etl.find_last_value(db, db_col_loc_c, 'c')):
             if etl.xstr(row[0].value) == etl.xstr(row[2].value):
                 vals_changed.append(etl.xstr(row[2].value))
                 row[2].value = ''
 
-    return db, ref, 'Local Partner Agencies matched Implementing and were cleared\n' + ','.join(vals_changed)
+    return db, ref, return_message('Local Partner Agencies matched Implementing and were cleared' ,vals_changed)
 
 def algo5(db,ref):
     #if Column C contains Column A: remove all spaces, commas, dashes and the matching substring in C
@@ -68,24 +73,24 @@ def algo5(db,ref):
     db_col_loc_c = etl.find_in_header(db, 'Local partner agency')
     vals_changed = []
 
-    for row in db.iter_rows(db_col_loc_a + "2:" + 
+    for row in db.iter_rows(db_col_loc_a + "2:" +
         etl.find_last_value(db, db_col_loc_c, 'c')):
             if etl.xstr(row[0].value) in etl.xstr(row[2].value) and etl.xstr(row[0].value) != etl.xstr(row[2].value):
                 vals_changed.append(row[2].value)
                 row[2].value = row[2].value.replace(row[0].value,'').replace(' ','').replace(',','').replace('-','')
 
-    return db, ref, 'IA contained in LPA for the following:\n' + ','.join(vals_changed)
+    return db, ref, return_message('IA contained in LPA for the following:' ,vals_changed)
 
 def algo6(db,ref):
     #Column G: must be in Reference>District
     db_col_loc = etl.find_in_header(db, 'District')
     ref_col_loc = etl.find_in_header(ref,'Admin1_District')
     missing_names = etl.colvals_notincol(db, db_col_loc, ref, ref_col_loc)
-    return db, ref, 'Districts not in the reference:\n' + ','.join(missing_names)
+    return db, ref, return_message('Districts not in the reference:' ,missing_names)
 
 def algo7(db,ref):
     #Column H: must be in Reference>VDC - relative to districy
-    #the district in which a vdc in the db is located must = the district in ref 
+    #the district in which a vdc in the db is located must = the district in ref
     db_dist_loc = etl.find_in_header(db, 'District')
     db_vdc_loc = etl.find_in_header(db, 'VDC / Municipalities')
     ref_dist_loc = etl.find_in_header(ref, 'Admin1_District')
@@ -93,18 +98,21 @@ def algo7(db,ref):
 
     #create tuple of vdc, dist for db and ref
     db_tuple = []
-    for row in db.iter_rows(db_dist_loc + "2:" + 
+    for row in db.iter_rows(db_dist_loc + "2:" +
         etl.find_last_value(db, db_vdc_loc, 'c')):
             db_tuple.append((etl.xstr(row[0].value), etl.xstr(row[1].value)))
 
     ref_tuple = []
     #do it just for dist, then for vdc as they're not neighbors
-    for row in ref.iter_rows('E2:' + 
+    for row in ref.iter_rows('E2:' +
         etl.find_last_value(ref, 'E', 'c')):
             ref_tuple.append((etl.xstr(row[0].value),))
- 
+
+
+
+
     it = 0
-    for row in ref.iter_rows(ref_vdc_loc + "2:" + 
+    for row in ref.iter_rows(ref_vdc_loc + "2:" +
         etl.find_last_value(ref, ref_vdc_loc, 'c')):
             ref_tuple[it]+=(etl.xstr(row[0].value.encode("utf8")),)
             it+=1
@@ -115,12 +123,12 @@ def algo7(db,ref):
         if tup not in ref_tuple:
             malformed_vdc.append(etl.xstr(tup))
 
-    return db, ref, 'VDCs in incorrect districts:\n' + ','.join(malformed_vdc)
+    return db, ref, return_message('VDCs in incorrect districts:' ,malformed_vdc)
 
 def algo8(db,ref):
     #Column I: must be in reference>Ward or a number or numbers separated by commas
-    
-    #take all into missing names and trim from there 
+
+    #take all into missing names and trim from there
     db_col_loc = etl.find_in_header(db, 'Municipal Ward')
     ref_col_loc = etl.find_in_header(ref,'Wards')
     missing_names = etl.colvals_notincol(db, db_col_loc, ref, ref_col_loc)
@@ -136,7 +144,7 @@ def algo8(db,ref):
                 break
 
 
-    return db, ref, 'Malformed wards: \n' + ','.join(invalid)
+    return db, ref, return_message('Malformed wards: ' ,invalid)
 
 def algo9(db,ref):
     #column J: must be in reference>Type of Activity
@@ -144,7 +152,7 @@ def algo9(db,ref):
     ref_col_loc = etl.find_in_header(ref,'Action_Type')
     missing_names = etl.colvals_notincol(db, db_col_loc, ref, ref_col_loc)
 
-    return db, ref, 'Incorrect Action types:\n' + ','.join(missing_names)
+    return db, ref, return_message('Incorrect Action types:' ,missing_names)
 
 def algo10(db,ref):
     #column J: must be in reference>ActionDescription relative to Action_Type
@@ -157,17 +165,17 @@ def algo10(db,ref):
     act_types_tups = []
 
     #go through action types and go through respected cols and make tuples
-    for row in ref.iter_rows(ref_col_loc + "2:" + 
+    for row in ref.iter_rows(ref_col_loc + "2:" +
         etl.find_last_value(ref, ref_col_loc, 'c')):
             act = etl.xstr(row[0].value)
             act_col = etl.find_in_header(ref, act)
-            for row in ref.iter_rows(act_col + "2:" + 
+            for row in ref.iter_rows(act_col + "2:" +
                 etl.find_last_value(ref, act_col, 'c')):
                     act_types_tups.append((act, etl.xstr(row[0].value)))
 
     #get tuples of dist actions
     type_desc_tup = []
-    for row in db.iter_rows(db_type_loc + "2:" + 
+    for row in db.iter_rows(db_type_loc + "2:" +
         etl.find_last_value(db, db_desc_loc, 'c')):
             type_desc_tup.append((etl.xstr(row[0].value),etl.xstr(row[1].value)))
 
@@ -177,7 +185,7 @@ def algo10(db,ref):
         if tup not in act_types_tups:
             malformed_acts.append(etl.xstr(tup))
 
-    return db, ref, 'Action descriptions not in action types:\n' + ','.join(malformed_acts)
+    return db, ref, return_message('Action descriptions not in action types:' ,malformed_acts)
 
 def algo11(db,ref):
     #Column L: must be in Reference>Target
@@ -185,14 +193,14 @@ def algo11(db,ref):
     ref_col_loc = etl.find_in_header(ref,'Target')
     missing_names = etl.colvals_notincol(db, db_col_loc, ref, ref_col_loc)
 
-    return db, ref, 'Targets not in Referece\n' + ','.join(missing_names)
+    return db, ref, return_message('Targets not in Referece' ,missing_names)
 
 def algo12(db,ref):
     #Column M: must be a number>=0 and (more conditions to follow)
     db_col_loc = etl.find_in_header(db, '# Items / # Man-hours / NPR')
     bad_vals = []
 
-    for row in db.iter_rows(db_col_loc + "2:" + 
+    for row in db.iter_rows(db_col_loc + "2:" +
         etl.find_last_value(db, db_col_loc, 'c')):
             if not etl.xstr(row[0].value).isdigit():
                 if row[0].value == None:
@@ -200,14 +208,14 @@ def algo12(db,ref):
                 else:
                     bad_vals.append(etl.xstr(row[0].value))
 
-    return db, ref, 'Invalid numbers for # Items / # Man-hours / NPR\n' + ','.join(bad_vals)
+    return db, ref, return_message('Invalid numbers for # Items / # Man-hours / NPR' ,bad_vals)
 
 def algo13(db,ref):
     #Column N: must be a number>=0 and (more conditions to follow)
     db_col_loc = etl.find_in_header(db, 'Total Number Households')
     bad_vals = []
 
-    for row in db.iter_rows(db_col_loc + "2:" + 
+    for row in db.iter_rows(db_col_loc + "2:" +
         etl.find_last_value(db, db_col_loc, 'c')):
             if not etl.xstr(row[0].value).isdigit():
                 if row[0].value == None:
@@ -215,14 +223,14 @@ def algo13(db,ref):
                 else:
                     bad_vals.append(etl.xstr(row[0].value))
 
-    return db, ref, 'Invalid numbers for Total Number Households\n' + ','.join(bad_vals)
+    return db, ref, return_message('Invalid numbers for Total Number Households' ,bad_vals)
 
 def algo14(db,ref):
     #Column O: must be a number>=0 and (more conditions to follow)
     db_col_loc = etl.find_in_header(db, 'Average cost per households (NPR)')
     bad_vals = []
 
-    for row in db.iter_rows(db_col_loc + "2:" + 
+    for row in db.iter_rows(db_col_loc + "2:" +
         etl.find_last_value(db, db_col_loc, 'c')):
             if not etl.xstr(row[0].value).isdigit():
                 if row[0].value == None:
@@ -231,7 +239,7 @@ def algo14(db,ref):
                     bad_vals.append(etl.xstr(row[0].value))
 
 
-    return db, ref, 'Invalid numbers for Average cost per households (NPR)\n' + ','.join(bad_vals)
+    return db, ref, return_message('Invalid numbers for Average cost per households (NPR)' ,bad_vals)
 
 def algo15(db,ref):
     #Column P: must be a number>=0 and <= Column N (more conditions to follow)
@@ -241,13 +249,13 @@ def algo15(db,ref):
 
     #get count values
     cnt_vals = []
-    for row in db.iter_rows(db_cnt_loc + "2:" + 
+    for row in db.iter_rows(db_cnt_loc + "2:" +
         etl.find_last_value(db, db_cnt_loc, 'c')):
             cnt_vals.append(etl.xstr(row[0].value))
 
     #get female values
     fem_vals = []
-    for row in db.iter_rows(db_fem_loc + "2:" + 
+    for row in db.iter_rows(db_fem_loc + "2:" +
         etl.find_last_value(db, db_fem_loc, 'c')):
             fem_vals.append(etl.xstr(row[0].value))
 
@@ -259,7 +267,7 @@ def algo15(db,ref):
         elif int(vals[1]) > int(vals[0]):
             malformatted.append(etl.xstr(vals))
 
-    return db, ref, 'Total #HH and Female #HH conflict or issue \n' + ','.join(malformatted)
+    return db, ref, return_message('Total #HH and Female #HH conflict or issue ' ,malformatted)
 
 def algo16(db,ref):
     #Column Q: must be a number>=0 and <= Column N (more conditions to follow)
@@ -269,13 +277,13 @@ def algo16(db,ref):
 
     #get count values
     cnt_vals = []
-    for row in db.iter_rows(db_cnt_loc + "2:" + 
+    for row in db.iter_rows(db_cnt_loc + "2:" +
         etl.find_last_value(db, db_cnt_loc, 'c')):
             cnt_vals.append(etl.xstr(row[0].value))
 
     #get female values
     vul_vals = []
-    for row in db.iter_rows(db_vul_loc + "2:" + 
+    for row in db.iter_rows(db_vul_loc + "2:" +
         etl.find_last_value(db, db_vul_loc, 'c')):
             vul_vals.append(etl.xstr(row[0].value))
 
@@ -287,7 +295,7 @@ def algo16(db,ref):
         elif int(vals[1]) > int(vals[0]):
             malformatted.append(etl.xstr(vals))
 
-    return db, ref, 'Total #HH and Vulnerable #HH conflict or issue \n' + ','.join(malformatted)
+    return db, ref, return_message('Total #HH and Vulnerable #HH conflict or issue ' ,malformatted)
 
 def algo17(db,ref):
     #column R: must be in reference>Status
@@ -295,11 +303,11 @@ def algo17(db,ref):
     ref_col_loc = etl.find_in_header(ref,'Status')
     missing_names = etl.colvals_notincol(db, db_col_loc, ref, ref_col_loc)
 
-    return db, ref, 'Activity Status not in Referece\n' + ','.join(missing_names)
+    return db, ref, return_message('Activity Status not in Referece' ,missing_names)
 
 def algo18(db,ref):
     #Column S: must be a date>= 25/04/2015
-    date_col_loc = etl.find_in_header(db, 'Start date')
+    date_col_loc = etl.find_in_header(db, 'Start Date')
 
     bad_date = []
 
@@ -313,18 +321,18 @@ def algo18(db,ref):
         except:
             bad_date.append(cur + ' @ cell ' + row[0].coordinate)
 
-    return db, ref, 'Malformatted date\n' + ','.join(bad_date)
+    return db, ref, return_message('Malformatted date' ,bad_date)
 
 
 
 def algo19(db,ref):
     #Column T: must be a date>= 25/04/2015 and >=Column S
     comp_col_loc = etl.find_in_header(db, 'Completion Date')
-    start_col_loc = etl.find_in_header(db, 'Start date')
+    start_col_loc = etl.find_in_header(db, 'Start Date')
 
     bad_date = []
 
-    for row in db.iter_rows(comp_col_loc + "2:" + 
+    for row in db.iter_rows(comp_col_loc + "2:" +
         etl.find_last_value(db, comp_col_loc, 'c')):
             cur = etl.xstr(row[0].value)
             try:
@@ -333,7 +341,7 @@ def algo19(db,ref):
             except Exception, e:
                 bad_date.append(cur + ' @ cell ' + row[0].coordinate)
 
-    return db, ref, 'Malformatted date or before start date\n' + ','.join(bad_date)
+    return db, ref, return_message('Malformatted date or before Start Date' ,bad_date)
 
 def algo20(db,ref):
     #Output if completion date > current date and activity status not like completed
@@ -351,126 +359,126 @@ def algo20(db,ref):
                 except:
                     Exception('Bad date formatting for: ' + str(cur_date))
 
-            if cur_date < datetime.now() and 'completed' not in v[0].value.lower():
-                bad_date.append('%s row %i' % (str(datetime.date(cur_date)),i))
+            try:
+                if cur_date < datetime.now() and 'completed' not in v[0].value.lower():
+                    bad_date.append('%s row %i' % (str(datetime.date(cur_date)),i))
+            except:
+                    bad_date.append('%s row %i' % (str(cur_date),i))
 
-    return db, ref, 'Completion date has passed for \n' + ','.join(bad_date)
+    return db, ref, return_message('Bad date or Completion Date has passed for ' ,bad_date)
 
 def algo21(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo22(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo23(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo24(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo25(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo26(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo27(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo28(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo29(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo30(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo31(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo32(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo33(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo34(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo35(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo36(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo37(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo38(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo39(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo40(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo41(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo42(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo43(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo44(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo45(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo46(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo47(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
 def algo48(db,ref):
     #
-    return db, ref, '\n' + ','.join()
+    return db, ref, return_message('' ,)
 
-def algo49(db,ref):
-    #
-    return db, ref, '\n' + ','.join()
 
 
 def algo(db,ref):
--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ setup(name='etl',
       author='Ewan Oglethorpe',
       author_email='ewanogle@gmail.com',
       packages = ['etl'], 
-      install_requires = ['dropbox', 'openpyxl','click', 'sqlalchemy','python-dateutil','psycopg2']
+      install_requires = ['dropbox', 'openpyxl','click', 'sqlalchemy','python-dateutil','psycopg2'],
+	entry_points={
+    	'console_scripts': [
+		'etl = etl.etl:iterate_reports',
+   	 ]
+}
 )
